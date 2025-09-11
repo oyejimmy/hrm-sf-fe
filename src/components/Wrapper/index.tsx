3db5873caf1133e350e23399018f5407d8f92a5b
Jamil Ur Rahman <112857745+oyejimmy@users.noreply.github.com>
2025-09-11 11:41:13 +0500

    Update index.tsx
    updated the wrapper

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<{ isDarkMode: boolean }>`
   padding: 24px;
-  background-color: #f5f5f5;
+  background-color: ${({ isDarkMode }) => (isDarkMode ? '#141414' : '#f5f5f5')};
+  min-height: 100vh;
 `;
